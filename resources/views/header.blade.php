2789ba87a84300aaf5b8c84e8700f1b411ad1540
TasfiaPrima <55846643+TasfiaPrima@users.noreply.github.com>
2021-06-11 15:40:48 +0600

    logout

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,3 +1,9 @@
+<?php
+
+use App\Http\Controllers\ProductController;
+
+
+?>
 <nav class="navbar navbar-default">
     <div class="container-fluid">
         <!-- Brand and toggle get grouped for better mobile display -->
@@ -8,16 +14,16 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="#">Brand</a>
+            <a class="navbar-brand" href="/">hobby shop</a>
         </div>
 
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
             <ul class="nav navbar-nav">
                 <li class="active"><a href="#">Home </a></li>
-                <li class=""><a href="#">Order </a></li>
+                <li><a href="#">Order </a></li>
             </ul>
-            <form class="navbar-form navbar-left">
+            <form action="/search" class="navbar-form navbar-left">
                 <div class="form-group">
                     <input type="text" class="form-control" placeholder="Search">
                 </div>
@@ -25,6 +31,17 @@
             </form>
             <ul class="nav navbar-nav navbar-right">
                 <li><a href="#">Cart</a></li>
+                @if(Session::has('user'))
+                <li class="dropdown">
+                    <a class="dropdown-toggle" data-toggle="dropdown" href="#">{{Session::get('user')['name']}}
+                        <span class="caret"></span></a>
+                    <ul class="dropdown-menu">
+                        <li><a href="/logout">Logout</a></li>
+                    </ul>
+                </li>
+                @else
+                <li><a href="/login">login</a></li>
+                @endif
             </ul>
         </div><!-- /.navbar-collapse -->
     </div><!-- /.container-fluid -->
